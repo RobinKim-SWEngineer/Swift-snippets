0eadf558617d5890afef2213add2cc7657802723
Robin Kim <hgkim@seznam.cz>
2020-12-30 20:15:38 +0100

    Test commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import SQLite3
+
+class DatabaseTest {
+    var db: OpaquePointer?
+    
+    let FILE_URL = FileManager.default.urls(for: <#T##FileManager.SearchPathDirectory#>, in: <#T##FileManager.SearchPathDomainMask#>)
+    
+    
+}
